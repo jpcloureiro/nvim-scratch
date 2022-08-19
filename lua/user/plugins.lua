1e19f3fd858f5daf01f672c40d2d7c46fc21d38b
João Loureiro <loureiro.jpc@gmail.com>
2022-08-19 17:03:16 +0100

    (comment): pin latest stable version

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,7 +45,10 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
-  use "numToStr/Comment.nvim" -- Easily comment stuff
+  use {
+    'numToStr/Comment.nvim',
+    tag = 'v0.6.1'
+  } -- Easily comment stuff
   use "kyazdani42/nvim-web-devicons"
   use "kyazdani42/nvim-tree.lua"
   use "akinsho/bufferline.nvim"
